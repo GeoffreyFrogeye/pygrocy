dadec7fc1506bc5d00a83b8bc5a4ca1cac061d20
Geoffrey “Frogeye” Preud'homme <geoffrey@frogeye.fr>
2024-04-26 20:28:06 +0200

    Support pydantic 2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "requests",
         "backports.zoneinfo;python_version<'3.9'",  # backports can be removed when python 3.8 support is dropped
         "deprecation~=2.1.0",
-        "pydantic>=1.8.2,<1.11.0",
+        "pydantic>=2.6,<2.7.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
